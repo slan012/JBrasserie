7113f9518cc997394211afa6bee8881122102bd5
Etienne LARROUMETS <etienne.larroumets@etik.com>
2025-05-08 10:40:40 +0200

    Initial commit

---
+++ src/main/java/org/cnam/jbrasserie/App.java
@@ -0,0 +1,10 @@
+package org.cnam.jbrasserie;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Every body!");
+    }
+}
